bbd4642a74b02a8386b1eb398ca4c177e129ef9a
Akbar-07 <116638921+Akbar-07@users.noreply.github.com>
2024-12-28 07:53:11 +0500

    aa
    aa

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,3 +1,14 @@
 function click_nav() {
     document.querySelector(".click_nav").classList.toggle("click_nav_1")
 }
+
+function openModal(){
+    document.querySelector('.modal-container').style=`display:block`
+    document.querySelector('body').style=`overflow: hidden;`
+    document.querySelector('.container-form').style=`display:block`
+}
+function closeModal(){
+    document.querySelector('.modal-container').style=`display:none`
+    document.querySelector('body').style=`overflow: scroll;`
+    document.querySelector('.container-form').style=`display:none`
+}
